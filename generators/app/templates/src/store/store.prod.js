62cb84c1081b3ecf2ddf51befdd8bfe2a4794200
Halvard H. Eggen <halvard.eggen@cognite.com>
2017-12-05 16:51:57 +0100

    Add connected router to store

--- generators/app/templates/src/store/store.prod.js
+++ generators/app/templates/src/store/store.prod.js
@@ -1,10 +1,25 @@
-import { createStore, applyMiddleware } from 'redux'
-import ReduxThunk from 'redux-thunk'
-import rootReducer from 'reducers'
+import { createStore, applyMiddleware, compose } from 'redux';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
+import ReduxThunk from 'redux-thunk';
+import rootReducer from 'reducer';
 
-const middlewares = [ReduxThunk]
-const enhancer = [applyMiddleware(...middlewares)]
+// const logger = () => next => action => {
+//   if (action.type === '@@router/LOCATION_CHANGE') {
+//     global.mixpanel.track('Location change', action.payload);
+//   }
+//   const result = next(action);
+//   return result;
+// };
+
+export const history = createBrowserHistory();
+
+const middlewares = [routerMiddleware(history), ReduxThunk /** , logger */];
 
 export default function configureStore(initialState = {}) {
-  return createStore(rootReducer, initialState, ...enhancer)
+  return createStore(
+    connectRouter(history)(rootReducer),
+    initialState,
+    compose(applyMiddleware(...middlewares))
+  );
 }
