051259573f36016586da075a8e56bbd50b680639
Halvard H. Eggen <halvard.eggen@cognite.com>
2017-12-05 16:51:27 +0100

    Add connected router

--- generators/app/templates/src/routes/index.js
+++ generators/app/templates/src/routes/index.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import { CounterContainer } from 'containers'
-import { Header } from 'components'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import styled from 'styled-components'
+import React from 'react';
+import { CounterContainer } from 'containers';
+import { Header } from 'components';
+import { Route } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
+import styled from 'styled-components';
+import history from 'store/';
 
-const Container = styled.div`text-align: center;`
+const Container = styled.div`
+  text-align: center;
+`;
 
 function Routes() {
   return (
-    <Router>
+    <ConnectedRouter history={history}>
       <Container>
         <Header />
         <Route path="/" component={CounterContainer} />
       </Container>
-    </Router>
-  )
+    </ConnectedRouter>
+  );
 }
 
-export default Routes
+export default Routes;
