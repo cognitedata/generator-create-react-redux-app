7d6abc719ed310af18608432249f5481d7770841
Halvard H. Eggen <halvard.eggen@cognite.com>
2017-12-05 16:52:05 +0100

    Lint

--- generators/app/templates/src/styles/global-styles.js
+++ generators/app/templates/src/styles/global-styles.js
@@ -1,4 +1,4 @@
-import { injectGlobal } from 'styled-components'
+import { injectGlobal } from 'styled-components';
 
 /* eslint no-unused-expressions: 0 */
 injectGlobal`
@@ -13,4 +13,4 @@ injectGlobal`
     padding: 0;
     font-family: sans-serif;
   }
-`
+`;
