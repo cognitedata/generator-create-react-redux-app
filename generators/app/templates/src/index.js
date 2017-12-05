238bee8c0857ea759d48fef83c0be2519d3ad0a8
Halvard H. Eggen <halvard.eggen@cognite.com>
2017-12-05 16:51:43 +0100

    Add theme provider

--- generators/app/templates/src/index.js
+++ generators/app/templates/src/index.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import configureStore from 'store'
-import Routes from 'routes'
-import 'styles/global-styles'
-import registerServiceWorker from 'utils/registerServiceWorker'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import configureStore from 'store';
+import Routes from 'routes';
+import 'styles/global-styles';
+import registerServiceWorker from 'utils/registerServiceWorker';
+
+// TODO: create a cognite theme that is re-usable
+const theme = {};
 
 render(
   <Provider store={configureStore()}>
-    <Routes />
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root')
-)
-registerServiceWorker()
+);
+registerServiceWorker();
