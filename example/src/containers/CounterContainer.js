69a7f0ae077383ec85eea633e05479203e7f1719
Halvard H. Eggen <halvard.eggen@cognite.com>
2017-12-07 18:26:30 +0100

    Fix import bug

--- example/src/containers/CounterContainer.js
+++ example/src/containers/CounterContainer.js
@@ -1,53 +1,50 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Counter } from 'components'
-import { createStructuredSelector, createSelector } from 'reselect'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createStructuredSelector, createSelector } from 'reselect';
 
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import * as CounterActions from 'actions/counter'
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as CounterActions from 'modules/counter';
+import { Counter } from '../components/Counter';
 
 class CounterContainer extends React.Component {
   static propTypes = {
-    increment: PropTypes.func.isRequired,
-    decrement: PropTypes.func.isRequired,
-    incrementIfOdd: PropTypes.func.isRequired,
+    incrementCounter: PropTypes.func.isRequired,
+    decrementCounter: PropTypes.func.isRequired,
+    incrementCounterIfOdd: PropTypes.func.isRequired,
     counter: PropTypes.number.isRequired,
-  }
+  };
 
-  increment = () => {
-    this.props.increment()
-  }
+  incrementCounter = () => {
+    this.props.incrementCounter();
+  };
 
-  decrement = () => {
-    this.props.decrement()
-  }
+  decrementCounter = () => {
+    this.props.decrementCounter();
+  };
 
-  incrementIfOdd = () => {
-    this.props.incrementIfOdd()
-  }
+  incrementCounterIfOdd = () => {
+    this.props.incrementCounterIfOdd();
+  };
 
   render() {
     return (
       <Counter
         counter={this.props.counter}
-        increment={this.increment}
-        decrement={this.decrement}
-        incrementIfOdd={this.incrementIfOdd}
+        increment={this.incrementCounter}
+        decrement={this.decrementCounter}
+        incrementIfOdd={this.incrementCounterIfOdd}
       />
-    )
+    );
   }
 }
 
 const mapStateToProps = createStructuredSelector({
-  counter: createSelector(
-    (state) => state.counter,
-    (counterState) => counterState
-  ),
-})
+  counter: createSelector(state => state.counter, counterState => counterState),
+});
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators(CounterActions, dispatch)
+  return bindActionCreators(CounterActions, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
