dd53e23a1cc53403210201db20b9381bae69ee8b
Halvard H. Eggen <halvard.eggen@cognite.com>
2018-01-02 14:49:12 +0100

    Various minor fixes

--- generators/app/templates/src/containers/Counter/Counter.js
+++ generators/app/templates/src/containers/Counter/Counter.js
@@ -5,7 +5,7 @@ import { createStructuredSelector, createSelector } from 'reselect';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as CounterActions from 'modules/Counter';
-import { Counter } from '../components/Counter';
+import { Counter } from 'components/Counter';
 
 class CounterContainer extends React.Component {
   static propTypes = {
