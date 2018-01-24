563d57b3e24b4d805ec057fb1b2ddac5e495059f
Jessie Won <jessiewon@Jessies-MacBook-Pro-2.local>
2018-01-24 15:54:24 +0100

    add component index modify when generating new component

--- generators/app/templates/generators/component/index.js
+++ generators/app/templates/generators/component/index.js
@@ -1,86 +1,94 @@
-const componentExists = require('../utils/componentExists');
+const componentExists = require("../utils/componentExists");
 
 module.exports = {
-  description: 'Add an unconnected component',
+  description: "Add an unconnected component",
   prompts: [
     {
-      type: 'list',
-      name: 'type',
-      message: 'Select the type of component',
-      default: 'Stateless Function',
+      type: "list",
+      name: "type",
+      message: "Select the type of component",
+      default: "Stateless Function",
       choices: () => [
-        'Stateless Function (Pure)',
-        'Stateless Function',
-        'ES6 Class (Pure)',
-        'ES6 Class',
-      ],
+        "Stateless Function (Pure)",
+        "Stateless Function",
+        "ES6 Class (Pure)",
+        "ES6 Class"
+      ]
     },
     {
-      type: 'input',
-      name: 'name',
-      message: 'What should it be called?',
-      default: 'Button',
+      type: "input",
+      name: "name",
+      message: "What should it be called?",
+      default: "Button",
       validate: value => {
         if (/.+/.test(value)) {
           return componentExists(value)
-            ? 'A component with this name already exists'
+            ? "A component with this name already exists"
             : true;
         }
 
-        return 'The name is required';
-      },
-    },
+        return "The name is required";
+      }
+    }
   ],
   actions: data => {
     let componentTemplate;
 
     switch (data.type) {
-      case 'ES6 Class': {
-        componentTemplate = './component/es6.js.hbs';
+      case "ES6 Class": {
+        componentTemplate = "./component/es6.js.hbs";
         break;
       }
-      case 'ES6 Class (Pure)': {
-        componentTemplate = './component/es6.pure.js.hbs';
+      case "ES6 Class (Pure)": {
+        componentTemplate = "./component/es6.pure.js.hbs";
         break;
       }
-      case 'Stateless Function': {
-        componentTemplate = './component/stateless.js.hbs';
+      case "Stateless Function": {
+        componentTemplate = "./component/stateless.js.hbs";
         break;
       }
-      case 'Stateless Function (Pure)': {
-        componentTemplate = './component/stateless.pure.js.hbs';
+      case "Stateless Function (Pure)": {
+        componentTemplate = "./component/stateless.pure.js.hbs";
         break;
       }
       default: {
-        componentTemplate = './component/es6.js.hbs';
+        componentTemplate = "./component/es6.js.hbs";
       }
     }
 
-    const testTemplate = './component/spec.js.hbs';
-    const indexTemplate = './component/index.js.hbs';
+    const testTemplate = "./component/spec.js.hbs";
+    const indexTemplate = "./component/index.js.hbs";
 
     const actions = [
       {
-        type: 'add',
-        path: '../src/components/{{properCase name}}/{{properCase name}}.js',
+        type: "add",
+        path: "../src/components/{{properCase name}}/{{properCase name}}.js",
         templateFile: componentTemplate,
-        abortOnFail: true,
+        abortOnFail: true
       },
       {
-        type: 'add',
+        type: "add",
         path:
-          '../src/components/{{properCase name}}/{{properCase name}}.spec.js',
+          "../src/components/{{properCase name}}/{{properCase name}}.spec.js",
         templateFile: testTemplate,
-        abortOnFail: true,
+        abortOnFail: true
       },
       {
-        type: 'add',
-        path: '../src/components/{{properCase name}}/index.js',
+        type: "add",
+        path: "../src/components/{{properCase name}}/index.js",
         templateFile: indexTemplate,
-        abortOnFail: true,
+        abortOnFail: true
       },
+      {
+        type: "modify",
+        path: "../src/components/index.js",
+        pattern: /$(?![\r\n])/gm,
+        template:
+          "export { default as {{properCase name}} } from './{{properCase name}}';\n",
+        abortOnFail: true
+      }
     ];
 
     return actions;
-  },
+  }
 };
